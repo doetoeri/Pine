5ca012e362916218e43030f8f4bb2d8e841a3c02
doetoeri <doetoeri@goedu.kr>
2026-08-23 00:09:17 +0900

    chore: bump UI stability cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const PINCON_SW_VERSION = "20260823-ui1";
+const PINCON_SW_VERSION = "20260823-ui2";
 const PINCON_SHELL_CACHE = `pincon-shell-${PINCON_SW_VERSION}`;
 const PINCON_OLD_CACHE_PREFIXES = ["workbox-precache", "pincon-shell-"];
 
