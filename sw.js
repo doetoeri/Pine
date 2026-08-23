6c093016047cb3d55d5fd6e0fca046989bcf1d4d
doetoeri <doetoeri@goedu.kr>
2026-08-23 09:01:04 +0900

    chore: refresh app shell for floating ops dock

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const PINCON_SW_VERSION = "20260823-classic2";
+const PINCON_SW_VERSION = "20260823-dock1";
 const PINCON_SHELL_CACHE = `pincon-shell-${PINCON_SW_VERSION}`;
 const PINCON_OLD_CACHE_PREFIXES = ["workbox-precache", "pincon-shell-"];
 
