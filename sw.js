6996f4319e22dcff9d300380d8daa484e3432731
doetoeri <doetoeri@goedu.kr>
2026-08-23 10:26:14 +0900

    chore: bump stable PWA shell cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const PINCON_SW_VERSION = "20260823-smooth2";
+const PINCON_SW_VERSION = "20260823-smooth3";
 const PINCON_SHELL_CACHE = `pincon-shell-${PINCON_SW_VERSION}`;
 const PINCON_OLD_CACHE_PREFIXES = ["workbox-precache", "pincon-shell-"];
 
