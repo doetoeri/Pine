85d28232df2583e51371bc35fa45082d26d3e5fc
doetoeri <doetoeri@goedu.kr>
2026-08-28 16:01:33 +0900

    feat: start PinCon after account gate resolves

---
+++ next/app-bootstrap.js
@@ -0,0 +1,10 @@
+import { accountReady } from "./account-gate.js";
+
+await accountReady;
+await import("./reveal-loader.js");
+await import("./app.js");
+await import("./app-interactions.js");
+await import("./write-mode.js");
+await import("./admin-visibility.js");
+await import("./problem-bank.js");
+await import("./student-ops.js");
